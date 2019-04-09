5882428530acd7e5ce09c889111f8d02ca9429d5
Pranav Mehendiratta <mehendiratta@wisc.edu>
2019-04-09 17:44:12 -0500

    Everything moved attack.py. Need to print the correct output so baddecrypt works

--- attack.py
+++ attack.py
@@ -4,6 +4,8 @@
 # Outputs a modified ciphertext and tag
 
 import sys
+import Crypto.Cipher.AES
+import hashlib
 
 # Grab ciphertext from first argument
 ciphertextWithTag = (sys.argv[1]).decode("hex")
@@ -18,14 +20,12 @@ tag = ciphertextWithTag[len(ciphertextWithTag)-32:]
 
 # TODO: Modify the input so the transfer amount is more lucrative to
 # the recipient
-####### Put this in attack.py ###########
-
 origpt = \
 """AMOUNT: $  100.00
 Originating Acct Holder: Swift
 Orgininating Acct #82675-5823954
 
-I authorized the above amount to be transferred to the account #78561-1848
+I authorized the above amount to be transferred to the account #78561-1848 
 held by a UW-Student at the National Bank of the Cayman Islands.
 """
 
@@ -34,18 +34,12 @@ newpt = \
 Originating Acct Holder: Swift
 Orgininating Acct #82675-5823954
 
-I authorized the above amount to be transferred to the account #78561-1848
+I authorized the above amount to be transferred to the account #78561-1848 
 held by a UW-Student at the National Bank of the Cayman Islands.
 """
 
 origText = origpt[:16]
 newText = newpt[:16]
-
-print "origpt len: " + str(len(origpt))
-print "newpt len: " + str(len(newpt))
-
-print origText
-print newText
 
 origTextBytes = origText.encode("hex")
 origIntText = int(origTextBytes, 16)
@@ -53,14 +47,9 @@ origIntText = int(origTextBytes, 16)
 newTextBytes = newText.encode("hex")
 newIntText = int(newTextBytes, 16)
 
-print "origTextBytes: " + origTextBytes + ", origIntText: " + str(origIntText)
-print "newTextBytes: " + newTextBytes + ", newIntText: " + str(newIntText)
-
 origiv = ciphertextWithTag[:16]
 origivBytes = origiv.encode("hex")
 origivInt = int(origivBytes, 16)
-
-print "origivBytes: " + origivBytes + ", origivInt: " + str(origivInt)
 
 origCtInt = origIntText ^ origivInt
 newivInt = origCtInt ^ newIntText
@@ -69,12 +58,33 @@ newivList = list(hex(newivInt))
 newivBytes = ''.join(newivList[2:len(newivList) - 1])
 newivText = newivBytes.decode("hex")
 
-print "newivBytes -> " + newivBytes
-print "newivText -> " + newivText
-
 newTag = hashlib.sha256(newpt).digest()
-print "newTag len: " + str(len(newTag))
-####### Put this in attack.py ##########
 
 # TODO: Print the new encrypted message
-print ciphertext.encode("hex") + tag.encode("hex")
+output = newivText.encode("hex") + ciphertext[16:].encode("hex") + newTag.encode("hex") 
+#print output 
+
+f = open("testkeyfile", 'r' )
+key = f.readline()
+key = key[:32].decode("hex")
+f.close()
+
+# Grab ciphertext from first argument
+ciphertextWithTag = (output).decode("hex")
+
+if len(ciphertextWithTag) < 16+16+32:
+    print "Ciphertext is too short!"
+    sys.exit(0)
+
+iv = ciphertextWithTag[:16]
+ciphertext = ciphertextWithTag[:len(ciphertextWithTag)-32]
+tag = ciphertextWithTag[len(ciphertextWithTag)-32:]
+cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CBC, IV=iv )
+plaintext = cipher.decrypt( ciphertext[16:] )
+
+# Check the tag
+if tag.encode("hex") != hashlib.sha256(plaintext).hexdigest():
+    print "Invalid tag!"
+else:
+    print "Verified message:"
+    print plaintext
